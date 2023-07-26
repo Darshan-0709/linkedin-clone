61efa12e128949b0153daa7d7ac1009c84c0d693
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2023-07-26 16:35:57 +0530

    Update FeedOption.js

--- src/FeedOption.js
+++ src/FeedOption.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const FeedOption = ({ Icon, title, color }) => {
-  const className = `flex items-center text-${color} py-1 px-2 hover:bg-gray-200 rounded-md text-sm`
+  const className = `flex items-center ${color} py-1 px-2 hover:bg-gray-200 rounded-md text-sm`
   return (
     <button className={className}>
       <Icon />
-      <span className="pl-2 font-semibold">{title}</span>
+      <span className="max-[400px]:hidden pl-2 font-semibold">{title}</span>
     </button>
   );
 };
