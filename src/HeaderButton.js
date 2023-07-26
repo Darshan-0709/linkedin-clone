36a49ffa6903b72778ebc57ab0ecbe3f9279b7a6
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2023-07-26 16:36:05 +0530

    Update HeaderButton.js

--- src/HeaderButton.js
+++ src/HeaderButton.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const HeaderButton = ({ Icon, Title, profilePic, Arrow, onButtonClick }) => {
   return (
-    <button onClick={onButtonClick} className="flex flex-col items-center h-[3.4rem] relative text-gray-500 text-sm hover:text-black group transition-colors duration-100">
+    <button onClick={onButtonClick} className="flex flex-col items-center justify-center h-[3.4rem] relative text-gray-500 text-sm hover:text-black group transition-colors duration-100">
       <div className="absolute bottom-0 left-0 h-0.5 group-hover:w-full bg-black"></div>
-      {Icon && <Icon />}
-      {profilePic && <img className='w-6 h-6 rounded-full object-cover' src={profilePic} alt="My profile" /> }
+      {/* {Icon && <Icon />} */}
+      {profilePic ? <img className='w-6 h-6 rounded-full object-cover' src={profilePic} alt="My profile" /> : <Icon />}
       {Arrow ? (
-                <div className='p-0 items-center hidden text-sm grow min-[810px]:flex shrink relative'>
+                <div className='p-0 items-top hidden text-sm grow min-[810px]:flex  shrink relative'>
                   <span>{Title}</span> 
-                  {<Arrow/>}
+                  {<Arrow />}
                 </div>
               ) :
               <span className="hidden text-sm grow min-[810px]:inline">{Title}</span> }
