9d8bc50b89936edda57382cd97d221590d5fc7fe
Darshan <104707176+Darshan-0709@users.noreply.github.com>
2023-07-26 16:36:01 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,14 @@ import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdRhN0t-PYOz8Dz5cAUi-zatUnbppTPRk",
-  authDomain: "linkedin-clone-1b321.firebaseapp.com",
-  projectId: "linkedin-clone-1b321",
-  storageBucket: "linkedin-clone-1b321.appspot.com",
-  messagingSenderId: "92548893002",
-  appId: "1:92548893002:web:ea522ed643bc80399399b3"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth();
